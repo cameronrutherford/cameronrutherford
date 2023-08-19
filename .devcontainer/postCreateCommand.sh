64f8e57861f51b142d74d588e58aab992ef47176
cameronrutherford <cameron.rutherford@me.com>
2023-08-19 00:46:25 +0000

    Add pre-commit config to dev container.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-
-set -e
-set -x
-
-# Configure pre-commit hooks\
-# It would nice to have this be a part of the container, but it is nice to be able to disable
-# TODO: make a part of the container
-# mkdir only necessary in GitHub Actions??
-DEV_DIR=/workspaces/cameronrutherford
-mkdir -p $DEV_DIR && cd $DEV_DIR
-git config --global --add safe.directory $DEV_DIR
-pre-commit install-hooks
-
-# Configure git config so I can at least commit things
-# Should push/pull outside of a container for sure
+# Install username/email for convenience
+git config --global --add safe.directory /home/app
 git config --global user.email "cameron.rutherford@me.com"
 git config --global user.name "cameronrutherford"
-
-set +x
