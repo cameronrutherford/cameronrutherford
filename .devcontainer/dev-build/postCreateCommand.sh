a25bdeb8d7eb28e5efee0585426a070fa60ce95a
cameronrutherford <cameron.rutherford@me.com>
2023-10-21 17:30:53 +0000

    Add poetry configuration to pytthon env.

--- .devcontainer/dev-build/postCreateCommand.sh
+++ .devcontainer/dev-build/postCreateCommand.sh
@@ -4,3 +4,7 @@
 git config --global --add safe.directory /home/app
 git config --global user.email "cameron.rutherford@me.com"
 git config --global user.name "cameronrutherford"
+
+# Install pre-commit hooks
+cd /home/app
+pre-commit install --install-hooks
--- .devcontainer/docs-prebuilt/postCreateCommand.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install username/email for convenience
-git config --global --add safe.directory /home/app
-git config --global user.email "cameron.rutherford@me.com"
-git config --global user.name "cameronrutherford"
