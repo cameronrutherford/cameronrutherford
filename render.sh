822dd8f4cf1468c42fee73d09e3966bcf63a8230
Cameron Rutherford <cameron.rutherford@me.com>
2023-11-17 01:43:20 -0700

    Add one page resume. (#50)

--- render.sh
+++ render.sh
@@ -8,6 +8,8 @@ rm -rf ./quarto-cache &&
 mv ./docs ./quarto-cache &&
 quarto render resume/cpp_resume.qmd --profile cpp &&
 mv ./docs/resume/cpp_resume.pdf ./quarto-cache/resume &&
+quarto render resume/onepager_resume.qmd --profile onepager &&
+mv ./docs/resume/onepager_resume.pdf ./quarto-cache/resume &&
 quarto render resume/python_resume.qmd --profile python &&
 mv ./docs/resume/python_resume.pdf ./quarto-cache/resume &&
 quarto render resume/devops_resume.qmd --profile devops &&
