6a93c567b3ca254079b12d430e149dabf567be30
Cameron Rutherford <cameron.rutherford@me.com>
2024-04-28 17:39:03 -0400

    Changes to get WSL working (#63)
    * Add poetry config
    
    * Update Dockerfile to pass linting, change theme, and add poetry config
    
    * Update theme to darkly
    
    * Add light / dark toggle
    .
    
    * Get pre-commit to pass...
    
    * Remove yamllint file.
    
    * Use curl instead of wget.
    
    * Update README.md to have badges based on master.
    
    * Update pipeline badges to only show master.
    
    * Fix README with right branch for badges.
    
    * Only show one pipeline badge since it always will work...

--- render.sh
+++ render.sh
@@ -2,17 +2,19 @@
 
 # TODO - replace this with poethepoet / poetry, along with porting other aliases
 
-rm -rfd docs &&
+if [[ $1 -eq "-f" ]]; then
+	rm -rf ./docs
+fi
+
 quarto render &&
-rm -rf ./quarto-cache &&
-mv ./docs ./quarto-cache &&
-quarto render resume/cpp_resume.qmd --profile cpp &&
-mv ./docs/resume/cpp_resume.pdf ./quarto-cache/resume &&
-quarto render resume/onepager_resume.qmd --profile onepager &&
-mv ./docs/resume/onepager_resume.pdf ./quarto-cache/resume &&
-quarto render resume/python_resume.qmd --profile python &&
-mv ./docs/resume/python_resume.pdf ./quarto-cache/resume &&
-quarto render resume/devops_resume.qmd --profile devops &&
-mv ./docs/resume/devops_resume.pdf ./quarto-cache/resume &&
-rm -rf ./docs && mv ./quarto-cache ./docs && rm -rf ./quarto-cache &&
-quarto preview
+	mv ./docs ./quarto-cache &&
+	quarto render resume/cpp_resume.qmd --profile cpp &&
+	mv ./docs/resume/cpp_resume.pdf ./quarto-cache/resume &&
+	quarto render resume/onepager_resume.qmd --profile onepager &&
+	mv ./docs/resume/onepager_resume.pdf ./quarto-cache/resume &&
+	quarto render resume/python_resume.qmd --profile python &&
+	mv ./docs/resume/python_resume.pdf ./quarto-cache/resume &&
+	quarto render resume/devops_resume.qmd --profile devops &&
+	mv ./docs/resume/devops_resume.pdf ./quarto-cache/resume &&
+	rm -rf ./docs && mv ./quarto-cache ./docs && rm -rf ./quarto-cache &&
+	quarto preview --port 8080 --no-browser --host 0.0.0.0
