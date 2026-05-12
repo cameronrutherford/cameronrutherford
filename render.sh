ce9046190bb896782ffe0726cfd88ea01109dc97
Cameron Rutherford <cameron.rutherford@me.com>
2026-05-12 14:05:57 -0700

    Resume feedback - consolidated onepager (#97)
    * Add start of charliecloud support.
    
    * Update render.sh to work with local charliecloud instead of full install.
    
    * First bit of feedback.
    
    * Add quick render and preview for resume.
    
    * Final iteration of one-page resume with good formatting.
    
    * Revert skills section.

--- render.sh
+++ render.sh
@@ -1,9 +1,35 @@
 #!/bin/bash
 
-if [[ $1 -eq "-f" ]]; then
-	rm -rf ./docs
+case $1 in
+-q)
+  CMDS=("quarto render --profile onepager")
+  PRVW=("cmd.exe /C start $(wslpath -w $(realpath ./docs/resume/onepager_resume.pdf))")
+  ;;
+*)
+  CMDS=("quarto render" "quarto preview --port 8080 --host localhost")
+  echo "Doing a full render"
+  ;;
+esac
+
+PULL=false
+CHARLIECLOUD=true
+
+if $PULL == "true"; then
+  ch-image pull ghcr.io/cameronrutherford/quarto-ci
 fi
 
-quarto render --profile onepager &&
-quarto render &&
-quarto preview --port 8080 --no-browser --host 0.0.0.0
+RUN_CMD="ch-run ghcr.io/cameronrutherford/quarto-ci \
+  --bind=$PWD:/home/app/ -W -c /home/app \
+  --set-env=PATH=/bin:/usr/local/texlive/latest/bin/x86_64-linux:/quarto/bin -- "
+
+for CMD in "${CMDS[@]}"; do
+  if $CHARLIECLOUD == "true"; then
+    $RUN_CMD $CMD
+  else
+    $CMD
+  fi
+done
+
+if [ -n "$PRVW" ]; then
+  $PRVW
+fi
